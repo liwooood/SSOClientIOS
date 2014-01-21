74c4054c3f29bb2e6c840ea864513ad7ce02e662
chf <chf@chfmatoMacBook-Pro.local>
2014-01-21 22:08:08 +0800

    Initial Commit

---
+++ SSOClient/SSOClient.m
@@ -0,0 +1,13 @@
+//
+//  SSOClient.m
+//  SSOClient
+//
+//  Created by chf on 14-1-21.
+//  Copyright (c) 2014年 chf. All rights reserved.
+//
+
+#import "SSOClient.h"
+
+@implementation SSOClient
+
+@end
---
+++ SSOClientTests/SSOClientTests.m
@@ -0,0 +1,34 @@
+//
+//  SSOClientTests.m
+//  SSOClientTests
+//
+//  Created by chf on 14-1-21.
+//  Copyright (c) 2014年 chf. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SSOClientTests : XCTestCase
+
+@end
+
+@implementation SSOClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
